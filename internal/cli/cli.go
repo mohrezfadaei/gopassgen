8f9a815c0c065c35b5ee118c9c9607fa65067adc
Mohammad Reza Fadaei <mohrezfadaei@gmail.com>
2024-08-08 17:01:54 +0330

    add cli flags

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -8,12 +8,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var length int
+var excludeUppercase bool
+var excludeLowercase bool
+var excludeDigits bool
+var excludeSymbols bool
+var includeChars string
+
 var rootCmd = &cobra.Command{
 	Use:   "passgen-cli",
 	Short: "Password and Key Generator CLI",
 	Long:  "A command-line application to generate passwords and keys",
 	Run: func(cmd *cobra.Command, args []string) {
-		password, err := generator.GeneratePassword()
+		password, err := generator.GeneratePassword(length, excludeUppercase, excludeLowercase, excludeDigits, excludeSymbols, includeChars)
 		if err != nil {
 			log.Fatalf("Error generating password: %v", err)
 		}
@@ -25,4 +32,11 @@ func Execute() error {
 	return rootCmd.Execute()
 }
 
-func init() {}
+func init() {
+	rootCmd.Flags().IntVarP(&length, "length", "l", 16, "Length of the password")
+	rootCmd.Flags().BoolVar(&excludeUppercase, "no-uppercase", false, "Exclude uppercase letters")
+	rootCmd.Flags().BoolVar(&excludeLowercase, "no-lowercase", false, "Exclude lowercase letters")
+	rootCmd.Flags().BoolVar(&excludeDigits, "no-digits", false, "Exclude digits")
+	rootCmd.Flags().BoolVar(&excludeSymbols, "no-symbols", false, "Exclude symbols")
+	rootCmd.Flags().StringVar(&includeChars, "include-chars", "", "Include specific characters (incl., $@#$%^&*()_+{}|:\"<>?,./~`)")
+}
--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -2,24 +2,57 @@ package generator
 
 import (
 	"crypto/rand"
+	"errors"
 	"math/big"
+	"strings"
 )
 
 const (
-	passwordLength = 16
-	charset        = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	DefaultPasswordLength = 16
+	uppercaseLetters      = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lowercaseLetters      = "abcdefghijklmnopqrstuvwxyz"
+	digits                = "0123456789"
+	symbols               = "$@#$%^&*()_+{}|:\"<>?,./~`"
 )
 
-func GeneratePassword() (string, error) {
-	var password []byte
-	for i := 0; i < passwordLength; i++ {
+func GeneratePassword(length int, excludeUppercase, excludeLowercase, excludeDigits, excludeSymbols bool, includeChars string) (string, error) {
+	if length <= 0 {
+		length = DefaultPasswordLength
+	}
+
+	var charset string
+
+	if !excludeUppercase {
+		charset += uppercaseLetters
+	}
+	if !excludeLowercase {
+		charset += lowercaseLetters
+	}
+	if !excludeDigits {
+		charset += digits
+	}
+	if !excludeSymbols {
+		charset += symbols
+	}
+	if includeChars != "" {
+		charset += includeChars
+	}
+	charset += includeChars
+
+	if charset == "" {
+		return "", errors.New("no characters available to generate password")
+	}
+
+	var password strings.Builder
+	for i := 0; i < length; i++ {
 		char, err := randomChar(charset)
 		if err != nil {
 			return "", err
 		}
-		password = append(password, char)
+		password.WriteByte(char)
 	}
-	return string(password), nil
+
+	return password.String(), nil
 }
 
 func randomChar(charset string) (byte, error) {
